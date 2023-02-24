9c4849cc7ea4d4f35e4846b56759d0483903caee
Alevtina Kulakova <alya.kulakova1994@gmail.com>
2023-02-24 17:10:08 +0200

    adding buckets for backup

--- buckets.tf
+++ buckets.tf
@@ -1,8 +1,18 @@
 resource "aws_s3_bucket" "docker_images" {
-  bucket            = "docker-images-vertep-infra"
+  bucket = "docker-images-vertep-infra"
 }
 
 resource aws_s3_bucket_acl "docker_images_acl" {
   bucket = aws_s3_bucket.docker_images.id
   acl    = "authenticated-read"
 }
+
+resource "aws_s3_bucket" "backups" {
+  bucket = "backup-vertep-infra"
+}
+
+resource aws_s3_bucket_acl "backups_acl" {
+  bucket = aws_s3_bucket.backups.id
+  acl    = "authenticated-read"
+}
+
