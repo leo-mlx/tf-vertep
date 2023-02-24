a14e038d22e5d8e157e0b91fafbe4aa4036641a4
Alevtina Kulakova <alya.kulakova1994@gmail.com>
2023-02-24 18:00:35 +0200

    deleting backup bucket for now

--- buckets.tf
+++ buckets.tf
@@ -7,12 +7,12 @@ resource aws_s3_bucket_acl "docker_images_acl" {
   acl    = "authenticated-read"
 }
 
-resource "aws_s3_bucket" "backups" {
-  bucket = "backup-vertep-infra"
-}
+#resource "aws_s3_bucket" "backups" {
+#  bucket = "backup-vertep-infra"
+#}
 
-resource aws_s3_bucket_acl "backups_acl" {
-  bucket = aws_s3_bucket.backups.id
-  acl    = "authenticated-read"
-}
+#resource aws_s3_bucket_acl "backups_acl" {
+#  bucket = aws_s3_bucket.backups.id
+#  acl    = "authenticated-read"
+#}
 
